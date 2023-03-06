285f97a9d7b4cee02b5b8b16235481af9e839340
ravi16297 <ravikumar16297@gmail.com>
2023-03-06 21:34:29 +0530

    mayur pls confirm ater updating the addition.py wroking dirrctory file

--- addition.py
+++ addition.py
@@ -2,13 +2,14 @@ a = 25
 b = 50
 c = a + b
 print("Addition of numbers : " , c)
-
 #this is addiont of two numbers
-
-
-
-
-
-
-
 #add modify
+# I'm writing the same programme in another way by using class/user define function
+class add:
+    a=25
+    b=50
+    c=a+b
+    def new1(self):
+        print("valuse of c-",self.c)
+add()
+add().new1()
